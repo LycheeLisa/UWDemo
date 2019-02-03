2daca68be820c3a706689c7202c11a4cf376bca2
Chen <jia.c.chen@accenture.com>
2019-02-03 03:25:57 -0500

    hi rotimi

--- UWaterloo STEM Demo/app/forms.py
+++ UWaterloo STEM Demo/app/forms.py
@@ -3,10 +3,10 @@ from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
 class InfoForm(FlaskForm):
-    gender = RadioField("Gender:", choices=[("F", "Female"), ("M", "Male")])
-    stem_classifier = RadioField("STEM Classifier:", choices=[("S","STEM"), ("N","Non-STEM")])
-    intl = RadioField("International or Domestic:", choices=[("I", "International"), ("D", "Domestic")])
-    UNIV101 = RadioField("UNIV101 Indicator:", choices=[("A", "Attended"), ("D", "Did not attend")])
-    ENGL109 = RadioField("ENGL109 Indicator:", choices=[("A", "Attended"), ("D", "Did not attend")])
+    gender = RadioField("Gender:", choices=[("0", "Female"), ("1", "Male")])
+    stem_classifier = RadioField("STEM Classifier:", choices=[("0","STEM"), ("1","Non-STEM")])
+    intl = RadioField("International or Domestic:", choices=[("0", "International"), ("1", "Domestic")])
+    UNIV101 = RadioField("UNIV101 Indicator:", choices=[("0", "Attended"), ("1", "Did not attend")])
+    ENGL109 = RadioField("ENGL109 Indicator:", choices=[("0", "Attended"), ("1", "Did not attend")])
     salary = StringField('Annual Salary')
     submit = SubmitField('Calculate Cluster')
--- UWaterloo STEM Demo/homepage.py
+++ UWaterloo STEM Demo/homepage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, request
 from app import app
 from app.forms import InfoForm
 from flask_bootstrap import Bootstrap
@@ -7,16 +7,19 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__, template_folder='templates')
 # bootstrap_app = Bootstrap(app)
 app.config['SECRET_KEY'] = 'asecretkey'
-@app.route('/', methods=['GET', 'POST'])
 
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    user = {"username": "Lisa"}
-    inputs = []
     form = InfoForm()
+    inputs = []
     if form.validate_on_submit():
         inputs.extend((form.gender.data, form.stem_classifier.data, form.intl.data, form.UNIV101.data, form.ENGL109.data, form.salary.data))
         flash('Data entered {}'.format(inputs))
-    return render_template("homepage.html", title = 'UWaterloo Demo', user=user, form=form)
+    return render_template("homepage.html", title = 'UWaterloo Demo',form=form, input=inputs)
 
+@app.route('/result', methods=['GET', 'POST'])
+def results():
+    results = request.form
+    print results
 if __name__ == "__main__":
     app.run(debug=True, port=9999)
