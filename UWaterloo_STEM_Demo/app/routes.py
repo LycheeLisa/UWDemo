8d5592499ce17afab85ebc8d1673ce6b4e57166c
Chen <jia.c.chen@accenture.com>
2019-10-16 12:28:05 -0400

    changed port and host name for ec2

--- UWaterloo_STEM_Demo/app/routes.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-@app.route('/')
-def home():
-    user = {"username": "Lisa"}
-    form = InfoForm()
-    return render_template("homepage.html", title = 'UWaterloo Demo', user=user, form=form)
